20fe4fb4907b221a5ded871e6798fc4e292be256
IKamyshew <ikamyshew@gmail.com>
2015-03-26 12:01:18 +0200

    Added heirs Automobile, Plane, Traine

---
+++ Vehicles/Vehicles/Automobile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    abstract class Automobile : Vehicle
+    {
+        private int wheels;
+
+        protected Automobile(String name, double speedPerHour, int safety, int capacity, int wheels)
+            : base(name, speedPerHour, capacity, safety)
+        {
+            this.wheels = wheels;
+        }
+    }
+}
