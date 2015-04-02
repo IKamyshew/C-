c9f46d77db65dee7c63955dad3b46211d5e050a5
IKamyshew <ikamyshew@gmail.com>
2015-04-02 11:34:33 +0300

    Created project for Client-Server trainings Started trainings according to http://habrahabr.ru/post/120157/ 1st step from article done.

---
+++ Client-ServerTrainings/Client-ServerTrainings/Server.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_ServerTrainings
+{
+    class Server
+    {
+        TcpListener Listener; // Объект, принимающий TCP-клиентов
+
+        // Запуск сервера
+        public Server(int Port)
+        {
+            // Создаем "слушателя" для указанного порта
+            Listener = new TcpListener(IPAddress.Any, Port);
+            Listener.Start(); // Запускаем его
+
+            // В бесконечном цикле
+            while (true)
+            {
+                // Принимаем новых клиентов и передаем их на обработку новому экземпляру класса Client
+                new Client(Listener.AcceptTcpClient());
+            }
+        }
+
+        // Остановка сервера
+        ~Server()
+        {
+            // Если "слушатель" был создан
+            if (Listener != null)
+            {
+                // Остановим его
+                Listener.Stop();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Создадим новый сервер на порту 80
+            new Server(80);
+        }
+    }
+}
