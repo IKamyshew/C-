33857243bd1359262d4402c1ae7b588f580b7245
IKamyshew <ikamyshew@gmail.com>
2015-04-06 13:22:11 +0300

    ASP.NET MVC 4 trainings BookStore (http://metanit.com/sharp/mvc/) Part 1 and 2 completed

---
+++ BookStore/BookStore/Models/BookDbInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BookStore.Models
+{
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "War and Peace", Author = "L.Tolstoj", Price = 220 });
+            db.Books.Add(new Book { Name = "Fathers and children", Author = "I.Turgenev", Price = 180 });
+            db.Books.Add(new Book { Name = "Seagull", Author = "A.Chehov", Price = 150 });
+            base.Seed(db);
+        }
+    }
+}
