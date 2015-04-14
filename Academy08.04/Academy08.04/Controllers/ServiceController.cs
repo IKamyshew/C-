f7910cfc4031f591dee2f3ddca6fd0c457e81f2f
IKamyshew <ikamyshew@gmail.com>
2015-04-14 17:28:38 +0300

    Added ServiceController / UserEdit

---
+++ Academy08.04/Academy08.04/Controllers/ServiceController.cs
@@ -0,0 +1,36 @@
+﻿using Academy08._04.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Academy08._04.Controllers
+{
+    [Authorize(Roles="Manager")]
+    public class ServiceController : Controller
+    {
+
+        AcademyContext db = new AcademyContext();
+
+        [HttpGet]
+        public ActionResult Groups()
+        {
+            ViewBag.Groups = db.Groups;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Groups(Group group)
+        {
+            if (!db.Groups.ToList().Contains(group)) // does't work! :(
+            {
+                db.Groups.Add(group);
+                db.SaveChanges();
+            }
+
+            return View();
+        }
+    }
+}
--- Academy08.04/Academy08.04/Controllers/UserController.cs
+++ Academy08.04/Academy08.04/Controllers/UserController.cs
@@ -19,7 +19,55 @@ namespace Academy08._04.Controllers
         public ActionResult Index()
         {
             var users = db.Users.Include(u => u.Group).Include(u => u.Role).ToList();
+
+            List<Group> groups = db.Groups.ToList();
+            groups.Insert(0, new Group { Name = "All", Id = 0 });
+            ViewBag.Groups = new SelectList(groups, "Id", "Name");
+
+            List<Role> roles = db.Roles.ToList();
+            roles.Insert(0, new Role { Id = 0, Name = "All" });
+            ViewBag.Roles = new SelectList(roles, "Id", "Name");
+
             return View(users);
+        }
+
+        [HttpPost]
+        public ActionResult Index(int group_filter, int role_filter)
+        {
+            IEnumerable<User> allUsers = null;
+            if (role_filter == 0 && group_filter == 0)
+            {
+                return RedirectToAction("Index");
+            }
+            if (role_filter == 0 && group_filter != 0)
+            {
+                allUsers = from user in db.Users.Include(u => u.Group).Include(u => u.Role)
+                           where user.GroupId == group_filter
+                           select user;
+            }
+            else if (role_filter != 0 && group_filter == 0)
+            {
+                allUsers = from user in db.Users.Include(u => u.Group).Include(u => u.Role)
+                           where user.RoleId == role_filter
+                           select user;
+            }
+            else
+            {
+                allUsers = from user in db.Users.Include(u => u.Group).Include(u => u.Role)
+                           where user.GroupId == group_filter && user.RoleId == role_filter
+                           select user;
+            }
+
+            List<Group> groups = db.Groups.ToList();
+            //Добавляем в список возможность выбора всех
+            groups.Insert(0, new Group { Name = "All", Id = 0 });
+            ViewBag.Groups = new SelectList(groups, "Id", "Name");
+
+            List<Role> roles = db.Roles.ToList();
+            roles.Insert(0, new Role { Name = "All", Id = 0 });
+            ViewBag.Roles = new SelectList(roles, "Id", "Name");
+
+            return View(allUsers.ToList());
         }
 
         [HttpGet]
@@ -38,6 +86,47 @@ namespace Academy08._04.Controllers
         public ActionResult Create(User user)
         {
             db.Users.Add(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Authorize(Roles="Manager")]
+        public ActionResult Edit(int id)
+        {
+            User user = db.Users.Find(id);
+            SelectList groups = new SelectList(db.Groups, "Id", "Name", user.GroupId);
+            ViewBag.Groups = groups;
+            SelectList roles = new SelectList(db.Roles, "Id", "Name", user.RoleId);
+            ViewBag.Roles = roles;
+
+            return View(user);
+        }
+
+        [HttpPost]
+        [Authorize(Roles="Manager")]
+        public ActionResult Edit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            SelectList groups = new SelectList(db.Groups, "Id", "Name");
+            ViewBag.Groups = groups;
+            SelectList roles = new SelectList(db.Roles, "Id", "Name");
+            ViewBag.Roles = roles;
+
+            return View(user);
+        }
+
+        [Authorize(Roles="Manager")]
+        public ActionResult Delete(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
