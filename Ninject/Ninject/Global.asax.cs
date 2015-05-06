9e6edcd99169cf271aa6b0a2435de67a94f22b1e
IKamyshew <ikamyshew@gmail.com>
2015-05-06 12:06:57 +0300

    Ninject added as global parameter

--- Ninject/Ninject/Global.asax.cs
+++ Ninject/Ninject/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Ninject.Infrastructure;
 
 namespace Ninject
 {
@@ -15,6 +16,8 @@ namespace Ninject
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
+            DependencyResolver.SetResolver(new NinjectDependencyResolver());
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
--- Ninject/Ninject/Models/IValueCalculator.cs
+++ Ninject/Ninject/Models/IValueCalculator.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace Ninject.Models
 {
-    interface IValueCalculator
+    public interface IValueCalculator
     {
-        decimal ValueProducts (IEnumerable<Product> products)
+        decimal ValueProducts(IEnumerable<Product> products);
     }
 }
