08e22bd8eef165f374916a3e6a5e36a31997c4d6
IKamyshew <ikamyshew@gmail.com>
2015-05-19 19:59:10 +0300

    no message

---
+++ TestTask/Academy/Academy.ASPNET/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Academy.Model.DBAccess;
+using Academy.Model.Entities;
+
+namespace Academy.ASPNET.Controllers
+{
+    public class UserController : Controller
+    {
+        Entities db = new Entities();
+
+        public ActionResult Index()
+        {
+            List<User> users = db.GetAllUsers();
+            return View();
+        }
+
+    }
+}
---
+++ TestTask/Academy/Academy.Model/DBAccess/Entities.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Academy.Model.Entities;
+using System.Data.Entity;
+
+namespace Academy.Model.DBAccess
+{
+    public class Entities
+    {
+        AcademyContext db = new AcademyContext();
+
+        public List<User> GetAllUsers()
+        {
+            return db.Users.Include(r => r.Role).Include(g => g.Group).ToList();
+        }
+
+        public List<User> GetUsersFilteredByRole(int roleId)
+        {
+            return db.Users.Include(r => r.Role).Include(g => g.Group).Where(r => r.RoleId == roleId).ToList();
+        }
+
+        public List<User> GetUsersFilteredByGroup(int groupId)
+        {
+            return db.Users.Include(r => r.Role).Include(g => g.Group).Where(g => g.GroupId == groupId).ToList();
+        }
+
+        public List<User> GetUsersFilteredByRoleAndGroup(int roleId, int groupId)
+        {
+            return db.Users.Include(r => r.Role).Include(g => g.Group).Where(r => r.RoleId == roleId).Where(g => g.GroupId == groupId).ToList();
+        }
+
+        public List<Role> GetAllRoles()
+        {
+            return db.Roles.ToList();
+        }
+    }
+}
